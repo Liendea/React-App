dfc2ef642b5529c2206874c0fed84974dd4de5c9
Linda Bengtsson <bengtsson-linda@outlook.com>
2025-05-14 11:59:32 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "../src/Components/Navbar/Navbar.jsx";
 import Sidebar from "../src/Components/Sidebar/Sidebar.jsx";
 import Hero from "../src/Components/Hero/Hero.jsx";
+import Footer from "../src/Components/Footer/Footer.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Sidebar></Sidebar>
         <Hero></Hero>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
